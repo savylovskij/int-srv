62ef58a7835696543d136629d31b2051b52de7ff
savylovskij_a <savylovskij@gmail.com>
2021-04-25 14:47:39 +0300

    feat(add-auth)
    * change auth.module
    
    #12

--- apps/backend/api/src/app/auth/auth.module.ts
+++ apps/backend/api/src/app/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { environment } from '../../environments/environment';
+import { PassportModule } from '@nestjs/passport';
+import { UserService } from '../users/services/user.service';
+import { AuthService } from './services/auth.service';
+import { PasswordService } from './services/password.service';
+import { JwtStrategy } from './services/jwt.strategy';
+import { AuthResolver } from './resolvers/auth.resolver';
 
-@Module({})
+@Module({
+  imports: [
+    UserService,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      privateKey: environment.jwt.secret,
+      signOptions: {
+        expiresIn: environment.jwt.expiresIn,
+      },
+    }),
+  ],
+  providers: [AuthService, PasswordService, JwtStrategy, AuthResolver],
+  exports: [AuthService, PassportModule],
+})
 export class AuthModule {}
