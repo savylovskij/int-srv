6dfdf79628a1c17d23bb82349fc322f59f61e983
savylovskij_a <savylovskij@gmail.com>
2021-04-25 15:12:17 +0300

    feat(add-auth)
    * change resolver
    
    #12

--- apps/backend/api/src/app/app.resolver.ts
+++ apps/backend/api/src/app/app.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import GraphQLJSON from 'graphql-type-json';
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
 
-@Resolver('app')
-export class AppResolver {}
+export const resolverMap = {
+  Data: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    serialize(value) {
+      return value.getTime(); // Value from client
+    },
+    parseValue(value) {
+      return new Date(value); // Value send to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+      }
+      return null; // Invalid hard-coded value (not an integer)
+    },
+  }),
+  JSON: GraphQLJSON,
+};
