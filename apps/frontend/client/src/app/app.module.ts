afbb2b1d76338579850f9d36b3f63a55cfff03e2
savylovskij_a <savylovskij@gmail.com>
2021-05-03 16:26:05 +0300

    feat(Store)
    * move lib

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { RootStoreModule } from '@int-srv/frontend/shared/data-access/root-store';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'intSrv-client' }),
+    RootStoreModule,
     RouterModule.forRoot([], {
       initialNavigation: 'enabled',
     }),
