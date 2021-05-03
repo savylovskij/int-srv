9f89f7f120c395168f7f107335628ccb77b8a9a7
savylovskij_a <savylovskij@gmail.com>
2021-05-03 20:45:37 +0300

    feat(user-store)
    * change user-store.module

--- libs/frontend/shared/data-access/user-store/src/lib/users-store.module.ts
+++ libs/frontend/shared/data-access/user-store/src/lib/users-store.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import * as fromUser from './+state/user.reducer';
 import { UserEffects } from './+state/user.effects';
-import { UserFacade } from './+state/user.facade';
 
 @NgModule({
   imports: [
-    CommonModule,
     StoreModule.forFeature(fromUser.USER_FEATURE_KEY, fromUser.reducer),
     EffectsModule.forFeature([UserEffects]),
   ],
-  providers: [UserFacade],
 })
-export class UserStoreModule {}
+export class UsersStoreModule {
+  static foRoot(
+    options: Partial<IUserStoreOptions>
+  ): ModuleWithProviders<UsersStoreModule> {
+    return {
+      ngModule: UsersStoreModule,
+      providers: [],
+    };
+  }
+}
+
+export interface IUserStoreOptions {
+  apollo: string;
+}
