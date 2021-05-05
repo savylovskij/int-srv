54c91c6eff8c81df335a6b8ec14148e40ed00ca0
savylovskij_a <savylovskij@gmail.com>
2021-05-05 22:43:04 +0300

    feat(store)
    * add start effect

--- libs/frontend/shared/data-access/user-store/src/lib/+state/user.effects.ts
+++ libs/frontend/shared/data-access/user-store/src/lib/+state/user.effects.ts
@@ -1,13 +1,50 @@
-import { Injectable } from '@angular/core';
-import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { fetch } from '@nrwl/angular';
-
-import * as UserFeature from './user.reducer';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { DataPersistence } from '@nrwl/angular';
+import { IUserStoreFeatureKey } from '../interfaces/user-store-feature-key.interface';
+import { IUserApollo } from '../interfaces/user-apollo.interface';
+import { createEffect } from '@ngrx/effects';
+import {
+  IActionEffectPayload,
+  IActionForcePayload,
+} from '@int-srv/shared/utils/interfaces';
 import * as UserActions from './user.actions';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { USER_FEATURE_KEY } from '@int-srv/frontend/shared/data-access/user-store';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class UserEffects {
-  init$ = createEffect(() =>
+  loadUser$ = createEffect(() =>
+    this.dataPersistence.fetch<IActionEffectPayload<IActionForcePayload>>(
+      UserActions.loadUser,
+      {
+        run(
+          action: IActionEffectPayload<IActionForcePayload>,
+          store?: IUserStoreFeatureKey
+        ): Observable<Action> | Action | void {
+          return isPlatformBrowser(this.platformId) &&
+            (!store[USER_FEATURE_KEY].userLoadRun || action.payload.force)
+            ? UserActions.loadUserRun
+            : UserActions.loadUserCancel;
+        },
+        onError(
+          action: IActionEffectPayload<IActionForcePayload>,
+          error: any
+        ): any {
+          console.log('error', error);
+        },
+      }
+    )
+  );
+
+  constructor(
+    private dataPersistence: DataPersistence<IUserStoreFeatureKey>,
+    private userApollo: IUserApollo,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
+
+  /*  init$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UserActions.init),
       fetch({
@@ -22,7 +59,5 @@ export class UserEffects {
         },
       })
     )
-  );
-
-  constructor(private actions$: Actions) {}
+  );*/
 }
