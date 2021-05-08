9b8e20e6811ee0cd5253d5fe247c53d2b4408798
savylovskij_a <savylovskij@gmail.com>
2021-05-08 19:35:18 +0300

    feat(storage)
    * change main module
    
    #23

--- libs/shared/utils/storage/src/lib/shared-utils-storage.module.ts
+++ libs/shared/utils/storage/src/lib/shared-utils-storage.module.ts
@@ -1,7 +1,47 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import {
+  ICookieService,
+  ICookieStorage,
+  ILocalStorage,
+  IMemoryStorage,
+  ISessionStorage,
+  IStorageOptions,
+} from '@int-srv/shared/utils/interfaces';
+import { BaseCookieService } from './services/base-cookie.service';
+import { BaseSessionStorage } from './storage/base-session.storage';
+import { BaseLocalStorage } from './storage/base-local.storage';
+import { BaseMemoryStorage } from './storage/base-memory.storage';
+import { BaseCookieStorage } from './storage/base-cookie.storage';
 
-@NgModule({
-  imports: [CommonModule],
-})
-export class SharedUtilsStorageModule {}
+@NgModule()
+export class StorageModule {
+  static foRoot(
+    options: Partial<IStorageOptions>
+  ): ModuleWithProviders<StorageModule> {
+    return {
+      ngModule: StorageModule,
+      providers: [
+        {
+          provide: ICookieService,
+          useClass: options.cookieService || BaseCookieService,
+        },
+        {
+          provide: ICookieStorage,
+          useClass: options.cookieStorage || BaseCookieStorage,
+        },
+        {
+          provide: ISessionStorage,
+          useClass: options.sessionStorage || BaseSessionStorage,
+        },
+        {
+          provide: ILocalStorage,
+          useClass: options.localStorage || BaseLocalStorage,
+        },
+        {
+          provide: IMemoryStorage,
+          useClass: options.memoryStorage || BaseMemoryStorage,
+        },
+      ],
+    };
+  }
+}
