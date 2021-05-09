e61a57764acd3bfbf2d2e7bd447e8d89f03085ae
savylovskij_a <savylovskij@gmail.com>
2021-05-09 15:00:41 +0300

    fix(some code)
    * fix

--- libs/shared/utils/interfaces/src/index.ts
+++ libs/shared/utils/interfaces/src/index.ts
@@ -1,8 +1,6 @@
-export * from './lib/entities/users.interface';
-export * from './lib/auth/sign.auth.interface';
 export * from './lib/apollo/apollo.interface';
-export * from './lib/store/action-force-payload.interface';
-export * from './lib/store/action-effect-payload.interface';
+export * from './lib/auth/sign.auth.interface';
+export * from './lib/entities/users.interface';
 export * from './lib/storage/abstract-storage.interface';
 export * from './lib/storage/cookie-service.interface';
 export * from './lib/storage/cookie-service-option.interface';
@@ -11,3 +9,5 @@ export * from './lib/storage/local-storage.interface';
 export * from './lib/storage/memory-storage.interface';
 export * from './lib/storage/session-storage.interface';
 export * from './lib/storage/storage-options.interface';
+export * from './lib/store/action-effect-payload.interface';
+export * from './lib/store/action-force-payload.interface';
