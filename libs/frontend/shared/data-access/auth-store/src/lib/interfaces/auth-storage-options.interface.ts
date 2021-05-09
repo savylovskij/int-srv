063b774b2604d60247edea6be81f783119f64b9a
savylovskij_a <savylovskij@gmail.com>
2021-05-09 20:31:46 +0300

    fix(auth)
    * add apollo queries

--- libs/frontend/shared/data-access/auth-store/src/lib/interfaces/auth-storage-options.interface.ts
+++ libs/frontend/shared/data-access/auth-store/src/lib/interfaces/auth-storage-options.interface.ts
@@ -1,6 +1,8 @@
 import { Type } from '@angular/core';
 import { IAuthStorage } from './auth-storage.interface';
+import { IAuthApollo } from './auth-apollo.interface';
 
 export interface IAuthStorageOptions {
   storage: Type<IAuthStorage>;
+  apollo: Type<IAuthApollo>;
 }
