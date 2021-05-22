517586c41b261ab2c28a6dfd95d5687b7c35f89c
savylovskij_a <savylovskij@gmail.com>
2021-05-22 15:57:32 +0300

    feat(auth)
    * add action

--- libs/frontend/shared/data-access/auth-store/src/lib/+state/auth.actions.ts
+++ libs/frontend/shared/data-access/auth-store/src/lib/+state/auth.actions.ts
@@ -1,14 +1,33 @@
-import { createAction, props } from '@ngrx/store';
-import { AuthEntity } from './auth.models';
+import { createAction } from '@ngrx/store';
+import { payload, payloadForce } from '@int-srv/shared/utils/store';
+import {
+  ISignAuthPayload,
+  ISignAuthResponse,
+} from '@int-srv/shared/utils/interfaces';
+import { ApolloError } from '@apollo/client';
 
-export const init = createAction('[Auth Page] Init');
-
-export const loadAuthSuccess = createAction(
-  '[Auth/API] Load Auth Success',
-  props<{ auth: AuthEntity[] }>()
+export const sigIn = createAction('[Auth] SigIn', payloadForce());
+export const sigInSet = createAction(
+  '[Auth] SigIn data set',
+  payload<ISignAuthPayload>()
+);
+export const sigInClear = createAction('[Auth] SigIn data clear');
+export const sigInCancel = createAction('[Auth] SigIn cancel');
+export const sigInRun = createAction('[Auth] SigIn run');
+export const sigInSuccess = createAction(
+  '[Auth] SigIn success',
+  payload<ISignAuthResponse>()
+);
+export const sigInFailure = createAction(
+  '[Auth] SigIn error',
+  payload<ApolloError>()
 );
 
-export const loadAuthFailure = createAction(
-  '[Auth/API] Load Auth Failure',
-  props<{ error: any }>()
+export const sigOut = createAction('[Auth] SigOut', payloadForce());
+export const sigOutCancel = createAction('[Auth] SigOut cancel');
+export const sigOutRun = createAction('[Auth] SigOut run');
+export const sigOutSuccess = createAction('[Auth] SigOut success');
+export const sigOutFailure = createAction(
+  '[Auth] SigOut error',
+  payload<ApolloError>()
 );
